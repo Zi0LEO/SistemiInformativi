849e3b3ca654ece8aed39fa278bb66e96fa8dcd2
thecoder <leonardo.npl.02@gmail.com>
2025-03-14 11:35:56 +0100

    reorganized imports, made methods static

--- OPCal_SI/src/main/java/com/opcal/model/DatiCliente.java
+++ OPCal_SI/src/main/java/com/opcal/model/DatiCliente.java
@@ -2,12 +2,12 @@ package com.opcal.model;
 
 public class DatiCliente extends Dati {
 
-  public DatiCliente(int ID, String nome, String cognome, String email, String passwd) {
+  public DatiCliente(String nome, String cognome, String email, String passwd) {
     super(nome, cognome, email, passwd);
   }
 
-  public DatiCliente(String email, String passwd) {
+/*  public DatiCliente(String email, String passwd) {
     super(null,null,email,passwd);
   }
-
+*/
 }
