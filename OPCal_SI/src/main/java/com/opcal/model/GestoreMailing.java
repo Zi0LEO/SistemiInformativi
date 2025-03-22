b2a47b72516945db648bbcb90ba5ba0cccbc11f1
umbertofrega <umbertofrega@gmail.com>
2025-03-22 16:25:30 +0100

    removed SendGrid dependency
    # Conflicts:
    #	OPCal_SI/src/main/java/com/opcal/model/GestoreMailing.java

--- OPCal_SI/src/main/java/com/opcal/model/GestoreMailing.java
+++ OPCal_SI/src/main/java/com/opcal/model/GestoreMailing.java
@@ -1,10 +1,12 @@
 package com.opcal.model;
 
 import com.opcal.Cliente;
-import com.opcal.Spedizione;
-import org.apache.torque.TorqueException;
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static com.opcal.model.GestoreClienti.getListaClienti;
@@ -16,8 +18,8 @@ public class GestoreMailing {
      * @return  Un'oggetto di tipo List che è la lista delle email, è inizializzata come ArrayList.
      */
     public static List<String> getMailingList(){
-        ArrayList<Cliente> lc = (ArrayList<Cliente>) getListaClienti();
-        ArrayList<String> mailingList = new ArrayList<>();
+        LinkedList<Cliente> lc = (LinkedList<Cliente>) getListaClienti();
+        LinkedList<String> mailingList = new LinkedList<>();
 
         for (Cliente cliente : lc)
             mailingList.add(cliente.getEmail());
@@ -25,15 +27,43 @@ public class GestoreMailing {
         return mailingList;
     }
 
-    public static void mandaAggiornamento(Cliente c){
-        List<Spedizione> spedizioni = new ArrayList<>();
-        try {
-            spedizioni = c.getSpedizioneRelatedByEmailDestinatarios();
-        } catch (TorqueException e) {
-            throw new RuntimeException(e);
+    public static void main(String[] args) {
+            send(costruisciMail(null,"Prova","Pizza"));
         }
 
-        
+
+    public static void aggiornaCliente(String email){
+        Email e = costruisciMail(email,"Spedizione","ciao bro");
+        send(e);
+
     }
 
+    private static Email costruisciMail(String email,String oggetto, String corpo){
+        String nomeCognome = "Umberto Frega";
+
+        return EmailBuilder.startingBlank()
+                .from("Servizio Clienti OPCAL","opcalsi@libero.it")
+                .to(nomeCognome,"umbertofrega@gmail.com")
+                .withSubject(oggetto)
+                .withPlainText(corpo)
+                .buildEmail();
+    }
+
+    public static void newsLetter(String corpo, String oggetto){
+        LinkedList<String> lista = (LinkedList<String>) getMailingList();
+        Email e = EmailBuilder.startingBlank()
+                .from("Servizio Clienti OPCAL","opcalsi@libero.it")
+                .toWithDefaultName("Gentile Cliente",lista)
+                .withSubject(oggetto)
+                .withPlainText(corpo)
+                .buildEmail();
+        send(e);
+    }
+
+    private static void send(Email e){
+        MailerBuilder.withSMTPServer("smtp.libero.it",587,"opcalsi@libero.it","EmailBuilder12!")
+                .buildMailer().sendMail(e);
+
+    }
+    //EmailBuilder12!
 }
