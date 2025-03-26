1ece408f1e00883ef8c81446369d7fec36452f49
umbertofrega <umbertofrega@gmail.com>
2025-03-26 11:20:27 +0100

    small fix

--- OPCal_SI/src/main/java/com/opcal/controller/AnnullaResoController.java
+++ OPCal_SI/src/main/java/com/opcal/controller/AnnullaResoController.java
@@ -7,7 +7,11 @@ import javax.swing.*;
 public class AnnullaResoController extends DialogController{
 
     public static void confermaAnnullamento(JDialog dialog,String codiceReso){
-        if(!GestoreResi.annullaReso(Integer.parseInt(codiceReso))){
+        if(!codiceReso.isEmpty()){
+            dialog.dispose();
+            return;
+        }
+        if(GestoreResi.annullaReso(Integer.parseInt(codiceReso))){
             dialog.dispose();
             JOptionPane.showMessageDialog(null,"Errore nell'annullamento del reso");
             return;
